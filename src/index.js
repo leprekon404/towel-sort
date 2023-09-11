6e3ea2d95cf8f02f6cb6b77dda08238d9a51fbf1
Anton <95173896+leprekon404@users.noreply.github.com>
2023-09-11 21:10:12 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+  if (matrix) {
+    return(
+    matrix.reduce((acc, cur, i) => {
+      cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
+      return acc;
+    }, []))
+  } else {
+    return arr;
+  }
 }
